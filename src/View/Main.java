3c11b362b1f1c7652af7d46e21f025114e6df2d8
José Renato Felix da Silva <93296913+joserenatofelix@users.noreply.github.com>
2023-04-10 10:08:38 -0300

    Update Main

--- src/View/Main.java
+++ src/View/Main.java
@@ -17,21 +17,20 @@ public class Main {
     
     public static void main(String[] args) {
         
-        String nome = "Renato";
-        System.out.println(nome);
-        
-        Servico servico = new Servico(1, "barba", 30);
+        Servico servico = new Servico(1,"Barba",20); 
         
         System.out.println(servico.getDescricao());
         System.out.println(servico.getValor());
-        
+            
         Cliente cliente = new Cliente(1, "Renato", "Rua Teste", "12321700");
-        System.out.println(cliente.getNome());
         
-        Usuario usuario = new Usuario(1, "barbeiro", "senha");
+	  System.out.println(cliente.getNome());
+        
+        Usuario usuario = new Usuario(2, "barbeiro", "1234", "2");
         System.out.println(usuario.getNome());
         
-        Agendamento agendamento = new Agendamento(1, cliente, servico, 30, "22/03/2023 09:20");
+        Agendamento agendamento = new Agendamento(1, cliente, servico, 30, "06/10/2023 09:20", "Observacao");
+
         System.out.println(agendamento.getCliente().getNome());
             
     }
